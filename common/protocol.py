0d5ba59b6f5549ca15145c5c0cedd9c57e5f7092
nylanderdev <rasmus.s.nylander@gmail.com>
2021-05-02 00:32:19 +0200

    Fix bug where generated headers are malformed. Close #6

--- common/protocol.py
+++ common/protocol.py
@@ -3,27 +3,31 @@ import math
 
 # Returns a header for a message of the specified length in the form of a byte array
 def generate_header(length):
+    # Quick case for empty messages, if required
+    if length == 0:
+        return [128]
     bytes_required = math.ceil(math.log2(length + 1) / 8)
     length_bytes = list(length.to_bytes(bytes_required, "big"))
-    bit_buffer = []
+    bit_padding_required = 7 - bytes_required * 8 % 7
+    bit_buffer = [False] * bit_padding_required
     header_bytes = []
-    expected_header_byte_length = math.ceil(len(length_bytes) * 8 / 7)
-    bit_padding_required = (expected_header_byte_length * 7 - len(length_bytes) * 8)
-    for i in range(0, bit_padding_required):
-        bit_buffer.append(False)
     for byte in length_bytes:
         bits = byte_to_bits(byte)
         for bit in bits:
             bit_buffer.append(bit)
             if len(bit_buffer) == 7:
                 assembled_byte = seven_bits_to_byte(bit_buffer)
-                header_bytes.append(assembled_byte)
+                # Skip any leading zero bytes, they are unnecessary
+                if len(header_bytes) > 0 or assembled_byte != 0:
+                    header_bytes.append(assembled_byte)
                 bit_buffer.clear()
+    # Toggle the MSB of the last byte
+    header_bytes[-1] += 0b1000_0000
     return header_bytes
 
 
 def seven_bits_to_byte(bits):
-    exponents = [1, 2, 4, 8, 16, 32, 64]
+    exponents = [64, 32, 16, 8, 4, 2, 1]
     byte = 0
     for position in range(0, 7):
         if bits[position]:
@@ -32,9 +36,9 @@ def seven_bits_to_byte(bits):
 
 
 def byte_to_bits(byte):
-    exponents = [1, 2, 4, 8, 16, 32, 64, 128]
+    exponents = [128, 64, 32, 16, 8, 4, 2, 1]
     bits = []
     for position in range(0, 8):
-        bit = exponents[7 - position] & byte
+        bit = exponents[position] & byte
         bits.append(bit != 0)
     return bits
