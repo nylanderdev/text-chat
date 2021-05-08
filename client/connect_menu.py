e3a3683d66155c12882bc7d2a9136d544afe6516
nylanderdev <rasmus.s.nylander@gmail.com>
2021-05-08 23:42:24 +0200

    Implement menu for selecting server, partially broken due to import issues. Incremental #21

---
+++ client/connect_menu.py
@@ -0,0 +1,37 @@
+from tkinter import *
+from socket import *
+# from connection import Connection
+
+
+def menu_main():
+    root = Tk()
+    root.title("Connect to server")
+    root.geometry("300x100")
+    Label(root, text="Server:").pack()
+    hostname_entry = Entry(root)
+    hostname_entry.pack()
+
+    def connect():
+        hostname = hostname_entry.get()
+        try:
+            # Attempt connection at port 1337
+            soc = socket(AF_INET, SOCK_STREAM)
+            soc.connect((hostname, 1337))
+            root.destroy()
+            # conn = Connection(soc)
+            # Todo: replace with function call, passing conn
+            import client
+        except:
+            # Failed to connect
+            hostname_entry.delete(0, END)
+            hostname_entry.insert(0, "Connection failed!")
+            pass
+
+    send_button = Button(root, text="Connect", bg="#5D92B1", fg="white", width=600, height=100, command=connect)
+    send_button.pack()
+    root.mainloop()
+    pass
+
+
+if __name__ == "__main__":
+    menu_main()
