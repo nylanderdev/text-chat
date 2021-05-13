e21d11e59c383da1f2892b77d32b3c1fc6d3e8a9
Your Name <you@example.com>
2021-05-13 22:37:21 +0200

    Added login/registeration fields and other functionality #61

--- client/connect_menu.py
+++ client/connect_menu.py
@@ -1,13 +1,14 @@
 from tkinter import *
 import tkinter.messagebox
 from socket import *
+from common import handle_user_info
 from .client import chat_client
 
 
 def menu_main():
     root = Tk()
     root.title("Connect to server")
-    root.geometry("300x200")
+    root.geometry("300x300")
     Label(root, text="Server:").pack()
     hostname_entry = Entry(root)
     hostname_entry.pack()
@@ -17,23 +18,59 @@ def menu_main():
     Label(root, text="Password:").pack()
     password_entry = Entry(root)
     password_entry.pack()
-    
-    def connect():
-        hostname = hostname_entry.get()
-        try:
-            # Attempt connection at port 1337
-            soc = socket(AF_INET, SOCK_STREAM)
-            soc.connect((hostname, 1337))
-            root.destroy()
-            chat_client(soc)
-        except:
-            # Failed to connect
-            hostname_entry.delete(0, END)
-            tkinter.messagebox.showinfo('Oops', 'Connection failed!')
-            pass
 
-    send_button = Button(root, text="Connect", bg="#5D92B1", fg="white", width=600, height=100, command=connect)
+    global login_check
+    login_check = False
+    global register_check
+    register_check = False
+
+    def connect():
+        if login_check or register_check == True:
+
+            hostname = hostname_entry.get()
+            try:
+                # Attempt connection at port 1337
+                soc = socket(AF_INET, SOCK_STREAM)
+                soc.connect((hostname, 1337))
+                root.destroy()
+                chat_client(soc)
+            except:
+                # Failed to connect
+                hostname_entry.delete(0, END)
+                tkinter.messagebox.showinfo('Oops', 'Connection failed!')
+                pass
+        else:
+            tkinter.messagebox.showinfo('Oops', 'You must login or register first!')
+
+    def authenticate_login():
+
+        handle_user_info.login(username_entry.get(), password_entry.get())
+        username_entry.delete(0, END)
+        password_entry.delete(0, END)
+        global login_check
+        login_check = True
+
+    def register():
+
+        handle_user_info.register(username_entry.get(), password_entry.get())
+        username_entry.delete(0, END)
+        password_entry.delete(0, END)
+        global register_check
+        register_check = True
+
+    divider = Label(root, width=600)
+    divider.pack()
+    login_button = Button(root, text="Login", bg="lightgray", fg="black", command=authenticate_login)
+    login_button.pack()
+    or_label = Label(root, text="or", width=600, font=("Helvetica", 12))
+    or_label.pack()
+    register_button = Button(root, text="register", bg="lightgray", fg="black", command=register)
+    register_button.pack()
+    divider = Label(root, width=600)
+    divider.pack()
+    send_button = Button(root, text="Connect", bg="#5D92B1", fg="white", width=600, height=200, command=connect)
     send_button.pack()
+
     root.mainloop()
     pass
 
