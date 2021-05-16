5511beeccffc9fce70790beb2b8fc88ab064c1b8
unknown <jmatsso@kth.se>
2021-05-16 23:22:13 +0200

    Made chatrooms clickable, and added functionality for them. Close #56

--- client/client.py
+++ client/client.py
@@ -14,6 +14,11 @@ def chat_client(soc):
     root.title('tk')
     connection = Connection(soc)
 
+    users = []
+    chatrooms = ["General", "Mem", "Inda"]
+    chatrooms_labels = []
+    current_room = 0
+
     def send():
         msg = message_box.get(1.0, END)
         message_box.delete(1.0, END)
@@ -44,6 +49,39 @@ def chat_client(soc):
             canvas_scroll.configure(scrollregion=canvas_scroll.bbox("all"))
             canvas_scroll.yview_moveto(1)
 
+    def change_rooms(num):
+        if num >= len(chatrooms):
+            print("To high chatroom index")
+        else:
+            #Delete old messages and chatrooms
+            for label in text_rectangle.winfo_children():
+                label.destroy()
+            text_labels = []
+            for label in chat_room.winfo_children():
+                label.destroy()
+            chatrooms_labels = []
+            #Make new rooms look nice
+            current_room = num
+            for i in range(0,len(chatrooms)):
+                room_label = Label(chat_room, font=("Helvetica", 10), bg="#17282A", fg="white", bd=5,
+                                       text=chatrooms[i])
+                if i == num:
+                    room_label = Label(chat_room, font=("Helvetica", 10), bg="#57282A", fg="white", bd=5,
+                                   text=chatrooms[i])
+                chatrooms_labels.append(room_label)
+                room_label.pack(side="top", fill="x", pady=1)
+                room_label.bind("<Button 1>",lambda e, numb=i: change_rooms(numb))
+
+    def update_users():
+        users = []
+        online_users_frame.configure(state=NORMAL)
+        online_users_frame.delete(1.0, END)
+        users = ["Test", "En annan", "Den tredje"]
+        for user in users:
+            online_users_frame.insert(END, user + "\n")
+        online_users_frame.configure(state=DISABLED)
+        root.after(100, update_users)
+
     def listen():
         receive()
         root.after(50, listen)
@@ -72,14 +110,20 @@ def chat_client(soc):
     text_labels = []
     scrollbar.place(relx=0.95, rely=0, relwidth=0.05, relheight=1.0)
 
-    # Chat room frame
+    #Chat room and user frames
     frame_upper_right = Frame(root, bd=5)
-    frame_upper_right.place(relx=0.7, rely=0.1, relwidth=0.3, relheight=0.5)
-    chat_room = Text(frame_upper_right, bd=1, font=("Helvetica", 14), bg="#17202A", fg="white")
-    chat_room.place(relx=0, rely=0, relwidth=1.0, relheight=0.5)
-    chat_room.configure(state=DISABLED)
-    online_users_frame = Text(frame_upper_right, bd=1, font=("Helvetica", 14), bg="#17202A", fg="white")
-    online_users_frame.place(relx=0, rely=0.5, relwidth=1.0, relheight=0.5)
+    frame_upper_right.place(relx=0.7, rely=0.1,relwidth=0.3,relheight=0.5)
+    chat_room = Frame(frame_upper_right, bd=1, bg="#17202A")
+    chat_room.place(relx=0, rely=0.05,relwidth=1.0,relheight=0.45)
+
+    online_users_frame = Text(frame_upper_right,font=("Helvetica", 8), bd=1, bg="#17202A", fg="white")
+    online_users_frame.place(relx=0, rely=0.55,relwidth=1.0,relheight=0.45)
+    online_users_frame.configure(state=DISABLED)
+
+    chat_room_label = Label(frame_upper_right,font=("Helvetica", 12), bd=1, bg="#17232A", fg="white", text="Chatrooms")
+    online_users_label = Label(frame_upper_right,font=("Helvetica", 12), bd=1, bg="#17232A", fg="white", text="Online Users")
+    chat_room_label.place(relx=0, rely=0,relwidth=1.0,relheight=0.05)
+    online_users_label.place(relx=0, rely=0.5,relwidth=1.0,relheight=0.05)
 
     # Message label
     message_label = Label(root, bd=5, text="Write your message below:", font=("Helvetica", 12), bg="#EAECAE")
@@ -100,5 +144,7 @@ def chat_client(soc):
     send_image = Button(frame_lower, text="send image", bg="#5D92B1", fg="white", command=send_img, bd=5)
     send_image.place(relx=0.5, rely=0.1, relwidth=0.2, relheight=0.8)
 
+    change_rooms(current_room)
+    update_users()
     listen()
     root.mainloop()
