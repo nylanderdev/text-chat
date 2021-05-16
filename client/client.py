bb1cf042a2e4235288ba0c26c30a594870d06e1a
unknown <jmatsso@kth.se>
2021-05-16 22:24:44 +0200

    Fixed scrollbar and some minor fixes. Close #69

--- client/client.py
+++ client/client.py
@@ -30,21 +30,19 @@ def chat_client(soc):
         img = Image.open(file_img)
         img.thumbnail((150,150))
         new_img = ImageTk.PhotoImage(img)
-        text_rectangle.configure(state=NORMAL)
-        text_rectangle.insert(1.0, "\n")
-        text_rectangle.image_create(1.0, image=new_img)
-        text_rectangle.configure(state=DISABLED)
-        text_rectangle.yview(END)
-
 
     def receive():
         msg_rec = connection.recv_plaintext()
+        user = "Temp"
         if msg_rec is not None:
             print(msg_rec)
-            text_rectangle.configure(state=NORMAL)
-            text_rectangle.insert(1.0, msg_rec)
-            text_rectangle.yview(END)
-            text_rectangle.configure(state=DISABLED)
+            text_label = Label(text_rectangle,font=("Helvetica", 12), bg="#17282A", fg="white" , bd=5, text=(user + ": " + msg_rec))
+            text_labels.append(text_label)
+            text_label.pack(side="top", fill="x", pady=1)
+            # Update scrollbar
+            canvas_scroll.update_idletasks()
+            canvas_scroll.configure(scrollregion=canvas_scroll.bbox("all"))
+            canvas_scroll.yview_moveto(1)
 
     def listen():
         receive()
@@ -57,12 +55,23 @@ def chat_client(soc):
     welcome_banner = Label(root, bd=5, bg="#17202A", fg="white", text="Welcome to this chatroom!",
                            font=("Ostrich Sans", 16, "bold"))
     welcome_banner.place(relx=0, rely=0, relwidth=1.0, relheight=0.1)
-    # Upper left frame
+
+    #Upper left frame, Scrollbar is a bit hacky
     frame_upper_left = Frame(root, bd=5)
-    frame_upper_left.place(relx=0, rely=0.1, relwidth=0.7, relheight=0.5)
-    text_rectangle = Text(frame_upper_left, font=("Helvetica", 14), bg="#17202A", fg="white")
-    text_rectangle.pack()
-    text_rectangle.configure(state=DISABLED)
+    frame_upper_left.place(relx=0, rely=0.1,relwidth=0.7,relheight=0.5)
+
+    canvas_scroll = Canvas(frame_upper_left, bg="#17202A")
+    canvas_scroll.pack(side=LEFT, fill="both", expand=1)
+    scrollbar = Scrollbar(frame_upper_left,orient=VERTICAL, command=canvas_scroll.yview)
+    scrollbar.pack(side=RIGHT, fill=Y)
+    canvas_scroll.configure(yscrollcommand=scrollbar.set)
+    canvas_scroll.bind("<Configure>", lambda e: canvas_scroll.configure(scrollregion=canvas_scroll.bbox("all")))
+
+    text_rectangle = Frame(canvas_scroll, bg="#17202A")
+    canvas_scroll.create_window((0,0), window=text_rectangle, anchor="nw", width=WIDTH*0.7)
+    text_labels = []
+    scrollbar.place(relx=0.95, rely=0, relwidth=0.05, relheight=1.0)
+
     # Chat room frame
     frame_upper_right = Frame(root, bd=5)
     frame_upper_right.place(relx=0.7, rely=0.1, relwidth=0.3, relheight=0.5)
@@ -71,6 +80,7 @@ def chat_client(soc):
     chat_room.configure(state=DISABLED)
     online_users_frame = Text(frame_upper_right, bd=1, font=("Helvetica", 14), bg="#17202A", fg="white")
     online_users_frame.place(relx=0, rely=0.5, relwidth=1.0, relheight=0.5)
+
     # Message label
     message_label = Label(root, bd=5, text="Write your message below:", font=("Helvetica", 12), bg="#EAECAE")
     message_label.place(relx=0, rely=0.6, relwidth=1.0, relheight=0.05)
